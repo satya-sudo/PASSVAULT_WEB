a0e066adb4302ef430f74b46aaee28389149821a
Satyam Shree <satyamshree780@gmail.com>
2020-12-13 23:50:26 +0530

    password reset client added

--- pms/settings.py
+++ pms/settings.py
@@ -135,4 +135,4 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER =  os.environ.get('EMAIL_HOST')
-EMAIL_HOST_PASSWORD = os.environ.get('EAMIL_HOST_PASS')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
