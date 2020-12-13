a4bff28edfd5d3e0bb2133f522b442cd4a902215
Satyam Shree <satyamshree780@gmail.com>
2020-12-13 23:54:19 +0530

    password reset client added

--- passvault/urls.py
+++ passvault/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth import  views as auth_views
 
 urlpatterns = [
     path("",views.index,name='index'),
+    path("TC",views.TC,name='TC'),
     path("login",views.login_view,name='login_view'),
     path("signup",views.register,name='register'),
     path('logout',views.logout_view,name='logout_view'),
