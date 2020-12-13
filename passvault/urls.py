a69b11c474f2c0fdaea83b9e653610a3fdd90497
Satyam Shree <satyamshree780@gmail.com>
2020-12-13 23:43:29 +0530

    password reset client added

--- passvault/urls.py
+++ passvault/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import  views as auth_views
+
 
 urlpatterns = [
     path("",views.index,name='index'),
-    path("TC",views.TC,name='TC'),
     path("login",views.login_view,name='login_view'),
     path("signup",views.register,name='register'),
     path('logout',views.logout_view,name='logout_view'),
@@ -11,4 +12,11 @@ urlpatterns = [
     path('AddCred',views.AddCred,name='AddCred'),
     path('search',views.search,name='search'),
     path('getAllPasswords',views.getallpassword,name='getallpassword'),
+
+    path('reset_password',auth_views.PasswordResetView.as_view(template_name="passvault/reset_1.html"),name="reset_password"),
+    path('resent_password_sent',auth_views.PasswordResetDoneView.as_view(template_name="passvault/reset_2.html"),name="password_reset_done"),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name="passvault/reset_3.html"),name="password_reset_confirm"),
+    path('reset_password_complete',auth_views.PasswordResetCompleteView.as_view(template_name="passvault/reset_4.html"),name="password_reset_complete"),
+
+
 ]
--- pms/settings.py
+++ pms/settings.py
@@ -127,3 +127,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 django_heroku.settings(locals())
+
+
+# SMTP CONFIG
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER =  os.environ.get('EMAIL_HOST')
+EMAIL_HOST_PASSWORD = os.environ.get('EAMIL_HOST_PASS')
